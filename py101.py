afdf7e912bc0edbc3452396c31a4f327b2227fc0
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-07-30 12:28:58 +0100

    Create py101.py

---
+++ py101.py
@@ -0,0 +1,72 @@
+print("hello world")
+x=int('123')
+string_one= "abc"
+string_two= "def"
+string_three= string_one+string_two
+
+print (type(123))
+print (type(str(123)))
+print (type(123.123))
+#print (type("true"))
+print (type("abc"))
+print (233>222)
+
+string_three[1:3]
+myArray = [1,2,3]
+myArray.append('4')
+
+string_three[1:3]
+myArray = [1,2,3]
+myArray.append('4')
+
+
+
+print (f'how {string_one}')#f-string
+
+print ('\'python\'') #escape charchter.
+print ('\nnew line ') #escape charchter.
+print(len(string_three))
+myArray2 = [1,2,3], [4,5,6]
+if 2>1:
+  print ("hello")
+else 
+  print ("bye")
+
+####
+a=10
+b=a
+id(a)
+id(b)
+####
+
+
+
+
+#function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python101.pythonlibrary.org
