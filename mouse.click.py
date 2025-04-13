c864de0b27490587bac048f9222a343d12dcc99c
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-13 22:22:10 +0100

    Create mouse.click.py

---
+++ mouse.click.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+# Delay so you can open the browser or app
+time.sleep(5)
+
+# Move mouse to a specific coordinate and click
+# Change (x, y) to your target location
+pyautogui.moveTo(3275, 235)  # X, Y pixel coordinates
+pyautogui.click()
