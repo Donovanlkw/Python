758970893f2dca46e0d66d76f3a7fde61618daef
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-14 16:17:41 +0100

    Update and rename PythonIni.ps1 to Python.ini.ps1

---
+++ Python.ini.ps1
@@ -0,0 +1,14 @@
+#https://www.python.org/downloads/windows/
+$path = "$env:LOCALAPPDATA\Programs\Python\Python313\Scripts\"
+$env:Path += ";$path"
+pip --version  
+
+<#
+
+pip install selenium
+python.exe -m pip --version
+python -m pip install -U selenium
+
+#https://sites.google.com/chromium.org/driver/downloads\
+Path = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
+#>
