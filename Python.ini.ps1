505cbe5676aa007bf8ce6e3b0a9dec827e2022ef
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-15 00:09:37 +0100

    Update Python.ini.ps1

--- Python.ini.ps1
+++ Python.ini.ps1
@@ -4,23 +4,10 @@ $env:Path += ";$path"
 $path1 = "$env:LOCALAPPDATA\Programs\Python\Python313\"
 $env:Path += ";$path1"
 
-
-
 pip --version  
-
-
-
 pip install pyautogui
 pip install time
-
-
-
-<#
-
 pip install selenium
+
 python.exe -m pip --version
 python -m pip install -U selenium
-
-#https://sites.google.com/chromium.org/driver/downloads\
-Path = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
-#>
