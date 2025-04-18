e67a566b60e34d722fb5f78f60b3a30e636157dc
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2025-04-18 23:24:56 +0100

    Update Python.ini.ps1

--- Python.ini.ps1
+++ Python.ini.ps1
@@ -1,16 +1,23 @@
 #https://www.python.org/downloads/windows/
-$path = "$env:LOCALAPPDATA\Programs\Python\Python313\Scripts\"
-$env:Path += ";$path"
-$path1 = "$env:LOCALAPPDATA\Programs\Python\Python313\"
-$env:Path += ";$path1"
+
+# New path you want to add
+$newPath1 = "$env:LOCALAPPDATA\Programs\Python\Python313\"
+$newPath2 = "$env:LOCALAPPDATA\Programs\Python\Python313\Scripts\"
+
+# Get current system PATH
+$sysPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+
+# Set updated system PATH
+$newPathValue = "$sysPath;$newPath1;$newPath2"
+[Environment]::SetEnvironmentVariable("PATH", $newPathValue, "Machine")
+
 
 pip --version  
 pip install pyautogui
 pip install time
 pip install selenium
-
 pip install playwright
-python -m playwright install
 
-python.exe -m pip --version
-python -m pip install -U selenium
+#python -m playwright install
+#python.exe -m pip --version
+#python -m pip install -U selenium
